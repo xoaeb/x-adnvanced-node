93e4bc7651a0f165e8e0ecc967d33541052c48df
xoaeb <shoaib.salim@npstx.com>
2024-07-09 14:13:28 +0530

    qtn statement for blocked thread

--- index.js
+++ index.js
@@ -6,50 +6,17 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-let items = [];
-let currentId = 1;
-
-// Create (POST)
-app.post("/items", (req, res) => {
-    const item = {
-        id: currentId++,
-        name: req.body.name,
-    };
-    items.push(item);
-    res.status(201).send(item);
+app.get("/nonBlocking", (req, res) => {
+    res.status(200).send("non blocking page");
 });
 
-// Read (GET)
-app.get("/items", (req, res) => {
-    res.send(items);
-});
-
-app.get("/items/:id", (req, res) => {
-    const item = items.find((i) => i.id === parseInt(req.params.id));
-    if (!item) {
-        return res.status(404).send("Item not found");
+app.get("/blocking", (req, res) => {
+    let counter = 0;
+    for (let index = 0; index < 10000000000; index++) {
+        counter++;
     }
-    res.send(item);
-});
 
-// Update (PUT)
-app.put("/items/:id", (req, res) => {
-    const item = items.find((i) => i.id === parseInt(req.params.id));
-    if (!item) {
-        return res.status(404).send("Item not found");
-    }
-    item.name = req.body.name;
-    res.send(item);
-});
-
-// Delete (DELETE)
-app.delete("/items/:id", (req, res) => {
-    const itemIndex = items.findIndex((i) => i.id === parseInt(req.params.id));
-    if (itemIndex === -1) {
-        return res.status(404).send("Item not found");
-    }
-    items.splice(itemIndex, 1);
-    res.status(204).send();
+    res.status(200).send(`result is ${counter}`);
 });
 
 app.listen(port, () => {
