64fbe7a78698a22293294d56d779f060e65d958b
xoaeb <shoaib.salim@npstx.com>
2024-07-15 00:27:42 +0530

    "Extracted Redis cache operations into separate utility functions and refactored cache retrieval and storage logic in /photos and /photos/:id routes"

--- index.js
+++ index.js
@@ -8,20 +8,18 @@ const Redis = require("redis");
 
 const redisClient = Redis.createClient({
     url: "redis://localhost:6380",
-}); //pass in {url:''} to push to production
+});
 
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
 
-// Connect to Redis
 redisClient.on("error", (err) => {
     console.error("Redis Client Error:", err);
-    process.exit(1); // Exit the process if Redis connection fails
+    process.exit(1);
 });
 
-// Connect to Redis
 redisClient.connect().catch((err) => {
     console.error("Failed to connect to Redis:", err);
-    process.exit(1); // Exit the process if Redis connection fails
+    process.exit(1);
 });
 
 DEFAULT_EXPIRATION = 3600; //EXPIRE AFTER 120s
@@ -53,7 +51,6 @@ app.get("/photos", async(req, res) => {
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: "Internal Server Error" });
-
     }
 });
 
@@ -63,13 +60,29 @@ app.get("/photos", async(req, res) => {
 // this returns around 35000 lines of data in about 250ms
 
 app.get("/photos/:id", async(req, res) => {
-    const { data } = await axios.get(
-        `
-                        https: //jsonplaceholder.typicode.com/photos/${req.params.id}`
-    );
-    res.json(data);
+    const photoId = req.params.id;
+
+    const photo = await redisClient.get(`photos?photoId=${photoId}`);
+
+    try {
+        if (photo) {
+            return res.status(200).json(JSON.parse(photo));
+        }
+        const { data } = await axios.get(
+            `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
+        );
+
+        redisClient.setEx(
+            `photos?photoId=${photoId}`,
+            DEFAULT_EXPIRATION,
+            JSON.stringify(data)
+        );
+        res.status(200).json(data);
+    } catch (error) {
+        console.log(error);
+    }
 });
-// this data in about 600ms
+// this fetches data in about 600ms
 
 app.listen(port, () => {
     console.log(`App running at http://localhost:${port}`);
