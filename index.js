a8c8e17552ee2dd6252b768f468a0df8019e3e1a
xoaeb <shoaib.salim@npstx.com>
2024-07-14 16:07:13 +0530

    "Updated index.js to use axios, removed worker threads, and updated package dependencies" created 2 dummy apis for getting photos from jsonplace holder

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { Worker } = require("worker_threads");
 const app = express();
 const port = 3000;
+const axios = require('axios');
 
-app.use(bodyParser.json());
-
-app.get("/nonBlocking", (req, res) => {
-    res.status(200).send("non blocking page");
+app.get("/photos", async(req, res) => {
+    const albumId = req.query.albumId;
+    const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/photos", { params: { albumId } }
+    );
+    res.json(data);
 });
 
-app.get("/blocking", (req, res) => {
-    const worker = new Worker("./worker.js");
-    worker.on("message", (data) => {
-        res.status(200).send(`result is ${data}`);
-    });
-    worker.on("error", (err) => {
-        res.status(404).send(`error is ${err}`);
-    });
-});
+// this returns around 35000 lines of data in about 250ms
 
-// When this route is accessed, it creates a new worker thread using the Worker class.
-// The worker thread runs the code in worker.js.
-// worker.on('message', ...) listens for messages from the worker thread. When the worker thread sends a message (the result of the computation), the server responds with that result.
-// worker.on('error', ...) listens for errors from the worker thread. If an error occurs, the server responds with an error message.
+app.get("/photos/:id", async(req, res) => {
+    const { data } = await axios.get(
+        `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
+    );
+    res.json(data);
+});
+// this data in about 600ms
 
 app.listen(port, () => {
     console.log(`App running at http://localhost:${port}`);
