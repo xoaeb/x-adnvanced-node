aa1f1fdec1d522fd75f8efa06aea5b5ca46cf88d
xoaeb <shoaib.salim@npstx.com>
2024-07-21 17:10:28 +0530

    hello world moleculer

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
-const port = 3000;
-const axios = require('axios');
+// CREATING MICROSERVICES USING MOLECULER
+//basic hello world
+import { ServiceBroker } from "moleculer";
 
-app.get("/photos", async(req, res) => {
-    const albumId = req.query.albumId;
-    const { data } = await axios.get(
-        "https://jsonplaceholder.typicode.com/photos", { params: { albumId } }
-    );
-    res.json(data);
+const broker = new ServiceBroker();
+
+broker.createService({
+    name: "greet",
+    actions: {
+        sayHello(ctx) {
+            return `Hello ${ctx.params.name}!`;
+        },
+    },
 });
 
-// this returns around 35000 lines of data in about 250ms
+async function startApp() {
+    await broker.start();
+    const res = await broker.call("greet.sayHello", { name: "shoaib" });
+    console.log(res);
+    broker.stop();
+}
 
-app.get("/photos/:id", async(req, res) => {
-    const { data } = await axios.get(
-        `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
-    );
-    res.json(data);
-});
-// this data in about 600ms
-
-app.listen(port, () => {
-    console.log(`App running at http://localhost:${port}`);
-});
+startApp();
